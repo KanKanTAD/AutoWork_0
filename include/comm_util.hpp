2a3c2d69a0e8f2d822ae8d7f462a312858a82aed
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-28 15:37:21 +0800

    1

--- include/comm_util.hpp
+++ include/comm_util.hpp
@@ -4,8 +4,14 @@
 
 #ifndef AUTOWORK0_COMM_UTIL_HPP
 #define AUTOWORK0_COMM_UTIL_HPP
+#include <list>
+#include <set>
 #include <string>
-namespace  comm_util {
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+
+namespace comm_util {
 
 #ifdef WIN32
 const char kSep = '\\';
@@ -13,11 +19,23 @@ const char kSep = '\\';
 const char kSep = '/';
 #endif
 
-
-bool semantic_equals(const std::string& a,const std::string& b){
-
+std::string strip(const std::string &s) {
+  std::string res = s;
+  boost::trim(res);
+  return res;
 }
 
+void split() { ; }
 
+template <typename Seq>
+std::string join(const Seq &seq, const std::string &s = "; ") {
+  auto &&res = boost::join(seq, s);
+  return res;
 }
+
+bool semantic_equals(const std::string &a, const std::string &b) {
+  return strip(a) == strip(b);
+}
+
+} // namespace comm_util
 #endif // AUTOWORK0_COMM_UTIL_HPP
--- main.cc
+++ main.cc
@@ -4,11 +4,10 @@
 
 #include "comm_util.hpp"
 #include <cassert>
-#include <hash_set>
-#include <set>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-
 /*
  *
  * auto_work bad_lib genreport -o xl_db.xlsx -d xl_db.xlsx
@@ -21,21 +20,53 @@
  * @param args
  * @return
  */
+struct Argument {
+  std::string tool;
+  std::string command;
+  std::vector<std::string> input_file;
+  std::string out_file;
+  std::string db_file;
 
-std::string tool;
-std::string command;
-std::string input_file;
-std::string out_file;
-std::string db_file;
-
-int main(int argc, char *argv[]) {
-  assert(argc >= 2);
-  tool = argv[1];
-  assert(argc >= 3);
-  command = argv[2];
-  int idx = 3;
-  while (true){
-
+  std::string str_;
+  std::string &str() {
+    std::stringstream ss;
+    ss << "tool:" << tool << std::endl;
+    ss << "command:" << command << std::endl;
+    ss << "out_file:" << out_file << std::endl;
+    ss << "db_file:" << db_file << std::endl;
+    ss << "input_files:" << comm_util::join(input_file) << std::endl;
+    str_ = ss.str();
+    return str_;
   }
+};
+
+void parse_args(int argc, char *argv[], Argument &arg) {
+  assert(argc >= 2);
+  arg.tool = argv[1];
+  assert(argc >= 3);
+  arg.command = argv[2];
+  for (int ix = 3; ix < argc;) {
+    if (comm_util::semantic_equals("-o", argv[ix])) {
+      ix++;
+      assert(ix < argc);
+      arg.out_file = argv[ix];
+      ix++;
+      continue;
+    }
+    if (comm_util::semantic_equals("-d", argv[ix])) {
+      ix++;
+      assert(ix < argc);
+      arg.db_file = argv[ix];
+      ix++;
+      continue;
+    }
+    arg.input_file.push_back(argv[ix]);
+    ix++;
+  }
+}
+int main(int argc, char *argv[]) {
+  Argument arg;
+  parse_args(argc, argv, arg);
+  std::cout << arg.str() << std::endl;
   return 0;
 }
