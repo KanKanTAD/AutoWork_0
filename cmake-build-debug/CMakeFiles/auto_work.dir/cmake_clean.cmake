95dd1f2be76df47e3e00b6daf4d8dd142b84ef11
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-28 13:44:51 +0800

    init 0

---
+++ cmake-build-debug/CMakeFiles/auto_work.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auto_work.dir/main.cc.obj"
+  "auto_work.exe"
+  "auto_work.exe.manifest"
+  "auto_work.lib"
+  "auto_work.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/auto_work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/auto_work.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for auto_work.
+# This may be replaced when dependencies are built.
